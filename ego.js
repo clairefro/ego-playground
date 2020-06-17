8cc70e4d197a94205cd91d230b30db92595865ae
claire froelich <claire.froelich@gmail.com>
2020-06-17 01:34:02 -0400

    init commit

---
+++ ego.js
@@ -0,0 +1,67 @@
+
+var ew = ["i'm", "i'd", "i've",'i','me','my','mine','myself'];
+var ww = ["we'd", "we've",'our','ours','ourselves'];
+var yw = ["you", "you'd", "you've",'your','yours','yourself','yourselves','y\'all', 'yous'];
+var tw = ["her", "she", "hers",'he','him','his','himself','herself', 'she\'d','he\'d', 'he\'s', 'she\'s'];
+var them = ["they've", "they'd", "they",'them','their','theirs','themselves'];
+
+var text = document.body.textContent;
+var body = document.querySelector('body');
+
+document.getElementById('getEgo').addEventListener('click', function () {
+  getEgoAndHighlightText(text, ew, 'red')
+  getEgoAndHighlightText(text, ww, 'yellow')
+  getEgoAndHighlightText(text, yw, 'green')
+  getEgoAndHighlightText(text, tw, 'blue')
+  getEgoAndHighlightText(text, them, 'purple')
+})
+
+function getEgoAndHighlightText(text, egoWords, color) {
+  getEgo(text, egoWords);
+  highlightText(body, egoWords, color);
+}
+
+function egoArrayToRegex(arr) {
+  var re = "";
+  arr.forEach(ew => re += '((\\b' + ew + '\\b)|(\\b\''+ ew +'[^\\w\'-]))|');
+  return re.slice(0,-1); // chop final '|'
+}
+
+function getEgo(text, egoWords) {
+  var wordCountAll = text.split(' ').length;
+
+  var countByEgoWord = {}
+  egoWords.forEach(function(egoWord) {
+    countByEgoWord[egoWord] = (text.match(new RegExp('\\b' + egoWord + '[^\w\'-]', 'gim')) || [] ).length
+  })
+
+  var wordCountEgo = Object.values(countByEgoWord).reduce((t,n) => t + n);
+  var ego = (wordCountEgo / wordCountAll) * 100
+  var egoWordBreakdown = {}
+
+  Object.keys(countByEgoWord).forEach(function(egoWord) {
+    egoWordBreakdown[egoWord] = {
+      count: countByEgoWord[egoWord],
+      rate: countByEgoWord[egoWord] / wordCountEgo,
+    }
+  })
+  var result = {
+    egoWords,
+    wordCountAll,
+    wordCountEgo,
+    ego,
+    egoWordBreakdown
+  }
+  console.log(result.egoWords)
+  console.log(result)
+  return result
+}
+
+function highlightText(element, egoWords, color) {
+
+  var innerHTML = element.innerHTML;
+
+  var regex = new RegExp('(' + egoArrayToRegex(egoWords) + ')','gim');
+
+  body.innerHTML= innerHTML.replace(regex, '<span style="background-color:' + color +';">$1</span>');
+}
